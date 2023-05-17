01ab447d02339f67f781235c8364d8a58285c0f0
Adrian_G <adriangarcia.comunicacion@gmail.com>
2023-05-17 17:44:32 +0200

    Delete App provisional test

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import App from './App';
-
-describe('Given the App component', () => {
-  test('Then provisional test should pass', () => {
-    render(<App />);
-    expect(true).toBe(true);
-  });
-});
