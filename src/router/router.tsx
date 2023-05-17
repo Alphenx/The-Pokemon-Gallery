5d3f085adbbb12a2406d4bc7f0491e684fdf1d9b
Adrian_G <adriangarcia.comunicacion@gmail.com>
2023-05-17 19:00:46 +0200

    Add Filtered page to router and SearchBar to layout

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import Home from '../pages/Home/Home';
 import Detail from '../pages/Detail/Detail';
 import MainLayout from '../layout/MainLayout';
+import Filtered from '../pages/Filtered/Filtered';
 
 const router = createBrowserRouter([
   {
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
       {
         path: '/:id',
         element: <Detail />,
+      },
+      {
+        path: '/filter/:type',
+        element: <Filtered />,
       },
     ],
   },
