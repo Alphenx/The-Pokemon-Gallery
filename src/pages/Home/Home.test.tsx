ed4b04ed8deaab5beac6318305552797093160ba
Adrian_G <adriangarcia.comunicacion@gmail.com>
2023-05-17 17:46:45 +0200

    Fix and rewrite Home tests

--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -1,32 +1,38 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import Home from './Home';
 import { MemoryRouter } from 'react-router-dom';
 
-describe('Home component', () => {
-  test('renders Loading component when loading is true', () => {
-    render(
-      <MemoryRouter>
-        <Home />
-      </MemoryRouter>
-    );
-    const loadingElement = screen.getByText(/Loading.../i);
-    expect(loadingElement).toBeInTheDocument();
-  });
-
-  test('renders ErrorMsg component when there is an error loading pokemons', async () => {
-    globalThis.fetch = jest.fn().mockResolvedValue({
-      ok: false,
-      json: jest.fn().mockResolvedValue(undefined),
+describe('Given a Home page', () => {
+  describe('When it is rendered', () => {
+    test('then it should render a Loading component', () => {
+      render(
+        <MemoryRouter>
+          <Home />
+        </MemoryRouter>
+      );
+      waitFor(() => {
+        const loadingElement = screen.getByText(/Loading.../i);
+        expect(loadingElement).toBeInTheDocument();
+      });
     });
 
-    render(
-      <MemoryRouter>
-        <Home />
-      </MemoryRouter>
-    );
-    const errorMsgElement = await screen.findByText(
-      /Ops...something went wrong. Please try again later./i
-    );
-    expect(errorMsgElement).toBeInTheDocument();
+    test('then it should render a ErrorMsg component when there is an error loading pokemons', async () => {
+      globalThis.fetch = jest.fn().mockResolvedValue({
+        ok: false,
+        json: jest.fn().mockResolvedValue(undefined),
+      });
+
+      render(
+        <MemoryRouter>
+          <Home />
+        </MemoryRouter>
+      );
+      await waitFor(() => {
+        const errorMsgElement = screen.getByText(
+          /Ops...something went wrong. Please try again later./i
+        );
+        expect(errorMsgElement).toBeInTheDocument();
+      });
+    });
   });
 });
